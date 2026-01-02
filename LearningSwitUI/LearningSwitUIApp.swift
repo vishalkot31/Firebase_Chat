828cbce0050af059542ccc4be24a74a0f02eec21
Vishal Kothari <vishal.chikki@gmail.com>
2026-01-02 15:12:53 +0530

    Initial Commit

---
+++ LearningSwitUI/LearningSwitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwitUIApp.swift
+//  LearningSwitUI
+//
+//  Created by Vishal Kothari on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
